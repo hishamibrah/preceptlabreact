ad2255941002abaed67d9bfb2b6873d16da61983
shamivul <shamivulruvaid@gmail.com>
2020-03-03 20:51:37 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React,{ Component } from 'react';
 import './App.css';
-import { Media, Button, Toast ,ToastHeader, ToastBody, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
-import Columns from 'react-columns';
+import { Button, Toast ,ToastHeader, ToastBody, InputGroup, InputGroupAddon, Input } from 'reactstrap';
 import Pagination from "react-js-pagination";
-
+import Grid from '@material-ui/core/Grid';
 
 class App extends Component {
   constructor(){
@@ -20,8 +19,8 @@ class App extends Component {
       handle:1
     }
   }
+
   async handlePageChange(pageNumber) {
-    console.log(`active page is ${pageNumber}`);
     this.setState({activePage: pageNumber,});
     let url = new URL('http://localhost:8080');
     url.search= new URLSearchParams({
@@ -32,10 +31,7 @@ class App extends Component {
     this.setState({
       datas:data.items
     })
-    console.log(this.state.datas);
   }
-
-
 
   async componentDidMount(){
     let url = new URL('http://localhost:8080');
@@ -47,13 +43,14 @@ class App extends Component {
     this.setState({
       datas:data.items
     })
-    console.log(this.state.datas);
   }
+
   handleBack(){
     this.setState({
       findUser:false
     })
   }
+
   async handleClick(){
     let url = new URL('http://localhost:8080/users');
     url.search= new URLSearchParams({
@@ -68,102 +65,106 @@ class App extends Component {
         handle:0
       })
     }else{
-        this.setState({
-          users:data.items,
-          findUser:true,
-          handle:1
-        })
-      }
+      this.setState({
+        users:data.items,
+        findUser:true,
+        handle:1
+      })
     }
-  onTodoChange(value){
-        this.setState({
-             search: value
-        });
-    }
+  }
 
+  onTodoChange(value){
+    this.setState({
+       search: value
+    });
+  }
 
   render(){
+    if(this.state.datas===undefined){
+      return(
+        <div className="box" style={{color:"red"}}><label>Api Limit exceded</label></div>
+      )
+    }else{
     if(this.state.findUser===true){
       return (
         <div>
-        <div className="box">
-        <Button classname="Button" onClick={this.handleBack.bind(this)}>Back</Button>
-        </div>
-        <div className="search">
-        <InputGroup>
-        <Input type="text" value={this.state.name}
-          onChange={e => this.onTodoChange(e.target.value)} placeholder="username" />
-        <InputGroupAddon addonType="apend">
-        <Button onClick={this.handleClick.bind(this)}>Search </Button>
-        </InputGroupAddon>
-        </InputGroup>
-
-        </div>
-        <div className="box">
-        {this.state.users.map((items,index) => (
-          <Toast style={{width:130}}>
-          <ToastHeader>
-          {items.login}
-          </ToastHeader>
-          <ToastBody>
-          <div>
-          <img
-          className="images"
-          src={items.avatar_url}
-          alt=""
-          />
+          <div className="box">
+            <Button className="Button" onClick={this.handleBack.bind(this)}>Back</Button>
           </div>
-          </ToastBody>
-          </Toast>
-        ))}
+          <div className="search">
+            <InputGroup>
+              <Input type="text" value={this.state.name}
+                onChange={e => this.onTodoChange(e.target.value)} placeholder="username" />
+              <InputGroupAddon addonType="prepend">
+                <Button onClick={this.handleClick.bind(this)}>Search </Button>
+              </InputGroupAddon>
+            </InputGroup>
         </div>
+        <div className="box">
+          {this.state.users.map((items,index) => (
+            <Toast key={items.id} style={{width:130}}>
+              <ToastHeader>
+                {items.login}
+              </ToastHeader>
+              <ToastBody>
+                <div>
+                  <img
+                    className="images"
+                    src={items.avatar_url}
+                    alt=""
+                  />
+                </div>
+              </ToastBody>
+            </Toast>
+          ))}
         </div>
+      </div>
       )}
       else{
-      return (
-        <div>
-        <div className="search">
-        <InputGroup>
-        <Input type="text" value={this.state.name}
-          onChange={e => this.onTodoChange(e.target.value)} placeholder="username" />
-        <InputGroupAddon addonType="apend">
-        <Button onClick={this.handleClick.bind(this)}>Search </Button>
-        </InputGroupAddon>
-        </InputGroup>
-        </div>
-        <div className="box">
-        <Pagination
-        itemClass="page-item"
-        linkClass="page-link"
-        activePage={this.state.activePage}
-        itemsCountPerPage={10}
-        totalItemsCount={1000}
-        pageRangeDisplayed={15}
-        onChange={this.handlePageChange.bind(this)}
-        />
-        <Columns columns="5">
-        {this.state.datas.map((items,index) => (
-          <Toast style={{width:130}}>
-          <ToastHeader>
-          {items.login}
-          </ToastHeader>
-          <ToastBody>
+        return (
           <div>
-          <img
-          className="images"
-          src={items.avatar_url}
-          alt=""
+            <div className="search">
+              <InputGroup>
+                <Input type="text" value={this.state.name}
+                  onChange={e => this.onTodoChange(e.target.value)} placeholder="username" />
+                  <InputGroupAddon addonType="prepend">
+                    <Button onClick={this.handleClick.bind(this)}>Search </Button>
+                  </InputGroupAddon>
+                </InputGroup>
+              </div>
+            <div className="box">
+          <Pagination
+            itemClass="page-item"
+            linkClass="page-link"
+            activePage={this.state.activePage}
+            itemsCountPerPage={10}
+            totalItemsCount={1000}
+            pageRangeDisplayed={15}
+            onChange={this.handlePageChange.bind(this)}
           />
-          </div>
-          </ToastBody>
-          </Toast>
-        ))}
-        </Columns>
-        </div>
-        </div>
-      );
-    }
-  }
+          <Grid container item={true}  justify="space-evenly" xs={10} >
+            {this.state.datas.map((items,index) => (
+              <Toast className="toast" key={items.id}>
+              <ToastHeader key={items.login}>
+                {items.login}
+              </ToastHeader>
+              <ToastBody>
+                <div>
+                  <img
+                    key={items.avatar_url}
+                    className="images"
+                    src={items.avatar_url}
+                    alt=""
+                  />
+                </div>
+              </ToastBody>
+            </Toast>
+          ))}
+        </Grid>
+      </div>
+    </div>
+    )}
+  }}
 }
 
 export default App;
