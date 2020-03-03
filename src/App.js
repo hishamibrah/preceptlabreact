4c4eacc689d80db743015e651bd9c5221c814429
shamivul <shamivulruvaid@gmail.com>
2020-03-03 17:48:00 +0530

    searches users

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ class App extends Component {
       totalResult:0,
       currentPage:1,
       totalPages:100,
-      activePage:1
+      activePage:1,
+      search:null,
+      users:[],
+      findUser:false,
+      handle:1
     }
   }
   async handlePageChange(pageNumber) {
@@ -31,6 +35,8 @@ class App extends Component {
     console.log(this.state.datas);
   }
 
+
+
   async componentDidMount(){
     let url = new URL('http://localhost:8080');
     url.search= new URLSearchParams({
@@ -43,54 +49,120 @@ class App extends Component {
     })
     console.log(this.state.datas);
   }
-
-  handleClick(){
-
+  handleBack(){
+    this.setState({
+      findUser:false
+    })
   }
+  async handleClick(){
+    let url = new URL('http://localhost:8080/users');
+    url.search= new URLSearchParams({
+      user: this.state.search
+    })
+    const response = await fetch(url);
+    const data = await response.json();
+    if(data.items===undefined){
+      alert("user not found")
+      this.setState({
+        findUser:false,
+        handle:0
+      })
+    }else{
+        this.setState({
+          users:data.items,
+          findUser:true,
+          handle:1
+        })
+      }
+    }
+  onTodoChange(value){
+        this.setState({
+             search: value
+        });
+    }
 
 
   render(){
-    return (
-      <div>
+    if(this.state.findUser===true){
+      return (
+        <div>
+        <div className="box">
+        <Button classname="Button" onClick={this.handleBack.bind(this)}>Back</Button>
+        </div>
         <div className="search">
-          <InputGroup>
-            <Input  placeholder="username" />
-            <InputGroupAddon addonType="apend">
-              <Button onClick={this.handleClick.bind(this)}>Search </Button>
-            </InputGroupAddon>
-          </InputGroup>
+        <InputGroup>
+        <Input type="text" value={this.state.name}
+          onChange={e => this.onTodoChange(e.target.value)} placeholder="username" />
+        <InputGroupAddon addonType="apend">
+        <Button onClick={this.handleClick.bind(this)}>Search </Button>
+        </InputGroupAddon>
+        </InputGroup>
+
         </div>
         <div className="box">
-          <Pagination
-            itemClass="page-item"
-            linkClass="page-link"
-            activePage={this.state.activePage}
-            itemsCountPerPage={10}
-            totalItemsCount={1000}
-            pageRangeDisplayed={15}
-            onChange={this.handlePageChange.bind(this)}
+        {this.state.users.map((items,index) => (
+          <Toast style={{width:130}}>
+          <ToastHeader>
+          {items.login}
+          </ToastHeader>
+          <ToastBody>
+          <div>
+          <img
+          className="images"
+          src={items.avatar_url}
+          alt=""
           />
-            <Columns columns="5">
-              {this.state.datas.map((items,index) => (
-              <Toast style={{width:130}}>
-                <ToastHeader>
-                  {items.login}
-                </ToastHeader>
-                <ToastBody>
-                  <div>
-                  <img
-                    className="images"
-                    src={items.avatar_url}
-                    alt=""
-                  />
-                  </div>
-                </ToastBody>
-              </Toast>
-              ))}
-            </Columns>
+          </div>
+          </ToastBody>
+          </Toast>
+        ))}
         </div>
-      </div>
-    );
+        </div>
+      )}
+      else{
+      return (
+        <div>
+        <div className="search">
+        <InputGroup>
+        <Input type="text" value={this.state.name}
+          onChange={e => this.onTodoChange(e.target.value)} placeholder="username" />
+        <InputGroupAddon addonType="apend">
+        <Button onClick={this.handleClick.bind(this)}>Search </Button>
+        </InputGroupAddon>
+        </InputGroup>
+        </div>
+        <div className="box">
+        <Pagination
+        itemClass="page-item"
+        linkClass="page-link"
+        activePage={this.state.activePage}
+        itemsCountPerPage={10}
+        totalItemsCount={1000}
+        pageRangeDisplayed={15}
+        onChange={this.handlePageChange.bind(this)}
+        />
+        <Columns columns="5">
+        {this.state.datas.map((items,index) => (
+          <Toast style={{width:130}}>
+          <ToastHeader>
+          {items.login}
+          </ToastHeader>
+          <ToastBody>
+          <div>
+          <img
+          className="images"
+          src={items.avatar_url}
+          alt=""
+          />
+          </div>
+          </ToastBody>
+          </Toast>
+        ))}
+        </Columns>
+        </div>
+        </div>
+      );
+    }
   }
 }
 
